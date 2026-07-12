ea37b884b37e42606c44af51e33c88df74b01bc0
hitaoaonhay-byte <hitaoaonhay@gmail.com>
2026-07-12 18:10:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -295,6 +295,41 @@ updateTimer();
 saveGame();
 }
 loadGame();
+async function saveOnline(){
+
+    if(playerName=="") return;
+
+    try{
+
+        await fetch("https://pet-world-production-3ca7.up.railway.app/save",{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                name:playerName,
+                coin:coin,
+                gem:gem,
+                level:level,
+                xp:xp,
+                food:food,
+                pettype:petType
+
+            })
+
+        });
+
+    }catch(err){
+
+        console.log(err);
+
+    }
+
+}
 function openShop(){
 
 document.getElementById("shop").style.display="block";
